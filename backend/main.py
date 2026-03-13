dbe0ea973444474e67d98ecee3764c86ae74fbb2
rohanghoghari111 <rohanghoghari56@gmail.com>
2026-03-13 08:57:53 +0530

    update files

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,14 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
+import requests
 
 app = FastAPI()
 
 locations = []
+
+TELEGRAM_TOKEN = "8521809713:AAF..."
+CHAT_ID = "7675394721"
 
 app.add_middleware(
     CORSMiddleware,
@@ -16,11 +20,34 @@ app.add_middleware(
 
 @app.post("/location")
 async def receive_location(data: dict):
+
     locations.append(data)
+
+    user = data.get("user")
+    lat = data.get("latitude")
+    lon = data.get("longitude")
+    battery = data.get("battery")
+
+    message = f"""
+🚨 Location Update
+
+User : {user}
+Latitude : {lat}
+Longitude : {lon}
+Battery : {battery}%
+"""
+
+    requests.post(
+        f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage",
+        data={"chat_id": CHAT_ID, "text": message}
+    )
+
     return {"status": "ok"}
+
 
 @app.get("/locations")
 async def get_locations():
     return locations
+
 
 app.mount("/", StaticFiles(directory="frontend", html=True), name="frontend")
